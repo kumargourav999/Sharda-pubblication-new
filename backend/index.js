888f489f4e47643c08ef63dbf3915bd10fc112ea
kumargourav999 <116900816+kumargourav999@users.noreply.github.com>
2026-03-10 21:17:23 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const journalRoutes = require(".//admin/controller/journalController");
 const app=express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/shardaPublication");
+//mongoose.connect("mongodb://localhost:27017/shardaPublication");
+app.use(cors());
+mongoose.connect("mongodb+srv://kumargourav:kumar123@cluster0.dxvytbg.mongodb.net/shardaPublication?retryWrites=true&w=majority&appName=Cluster0")
+//mongodb+srv://kumargourav:kumar123@cluster0.dxvytbg.mongodb.net/shardaPublication?retryWrites=true&w=majority&appName=Cluster0
 app.use("/api/admin",adminRoutes);
 app.use("/api/journals", journalRoutes);
 app.listen(5000,()=>{
