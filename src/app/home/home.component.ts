f5705aa47c0547691624ca286996af174449697d
kumargourav999 <116900816+kumargourav999@users.noreply.github.com>
2026-03-12 03:06:27 +0530

    compatible video

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  @ViewChild('homeVideo') video!: ElementRef<HTMLVideoElement>;
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    this.video.nativeElement.play();
+  }
 
 }
