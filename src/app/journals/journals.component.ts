6fa5d5afd838e5ade7ac28488061e5eb7bd669c6
kumargourav999 <116900816+kumargourav999@users.noreply.github.com>
2026-03-12 20:47:10 +0530

    journal

--- src/app/journals/journals.component.ts
+++ src/app/journals/journals.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { JournalServiceService } from '../service/journal-service.service';
 @Component({
   selector: 'app-journals',
   templateUrl: './journals.component.html',
   styleUrls: ['./journals.component.css']
 })
-export class JournalsComponent {
+export class JournalsComponent implements OnInit{
+journals:any[] = [];
+  selectedJournalIndex:number = -1;
 
+  constructor(private journalService:JournalServiceService){}
+
+  ngOnInit(){
+    this.getJournals();
+  }
+
+  getJournals(){
+    this.journalService.getJournals().subscribe((res:any)=>{
+      this.journals = res;
+    });
+  }
+
+  toggleJournal(index:number){
+    if(this.selectedJournalIndex === index){
+      this.selectedJournalIndex = -1;
+    }else{
+      this.selectedJournalIndex = index;
+    }
+  }
 }
